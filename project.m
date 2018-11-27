0c21ce525f7312a8cccb29e4e8ec38d9a992d02d
Nate Richman <nate.rich2@gmail.com>
2018-11-26 23:32:36 -0600

    Added initial GUIDE file

---
+++ project.m
@@ -0,0 +1,182 @@
+function varargout = project(varargin)
+% PROJECT MATLAB code for project.fig
+%      PROJECT, by itself, creates a new PROJECT or raises the existing
+%      singleton*.
+%
+%      H = PROJECT returns the handle to a new PROJECT or the handle to
+%      the existing singleton*.
+%
+%      PROJECT('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in PROJECT.M with the given input arguments.
+%
+%      PROJECT('Property','Value',...) creates a new PROJECT or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before project_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to project_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help project
+
+% Last Modified by GUIDE v2.5 26-Nov-2018 23:20:49
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @project_OpeningFcn, ...
+                   'gui_OutputFcn',  @project_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before project is made visible.
+function project_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to project (see VARARGIN)
+[file,path] = uigetfile('*.*');
+handles.image = imread(file);
+handles.image = imgaussfilt(handles.image,1);
+imshow(handles.image);
+
+% Choose default command line output for project
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes project wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = project_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on slider movement.
+function GrayThresh_Callback(hObject, eventdata, handles)
+% hObject    handle to GrayThresh (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+val = get(hObject,'Value');
+min = get(hObject,'Min');
+max = get(hObject,'Max');
+bw_image = ~im2bw(rgb2gray(handles.image),val);
+imshow(bw_image);
+handles.bw_image = bw_image;
+guidata(hObject,handles);
+
+
+
+% --- Executes during object creation, after setting all properties.
+function GrayThresh_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to GrayThresh (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+
+
+
+% --- Executes on slider movement.
+function slider6_Callback(hObject, eventdata, handles)
+% hObject    handle to slider6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+val = get(hObject,'Value');
+handles.cleaned_im = bwareaopen(handles.bw_image, double(int16(val)));
+imshow(handles.cleaned_im);
+guidata(hObject,handles);
+
+% --- Executes during object creation, after setting all properties.
+function slider6_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+im_filled = imfill(bwmorph(handles.cleaned_im,'bridge',inf),'holes');
+edges = find(handles.cleaned_im == 1);
+im_cells = im_filled;
+im_filled(edges) = 0;
+handles.im_filled = im_filled;
+handles.im_cells = im_cells;
+imshow(im_filled);
+guidata(hObject,handles);
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+regions = regionprops(handles.im_cells, 'Area','MajorAxisLength','MinorAxisLength');
+threshold = max([regions.Area])*.01;
+to_remove = find([regions.Area] < threshold);
+regions(to_remove) = [];
+for i = 1:length(regions)
+    regions(i).Roundness = regions(i).MajorAxisLength/regions(i).MinorAxisLength;
+end
+idx = kmeans([regions.Roundness]',3);
+unsplit = length(find(idx == 1));
+shmoo = length(find(idx == 2));
+dividing = length(find(idx == 3));
+
+count_string = sprintf('Normal: %d, Shmoo: %d, Dividing: %d', unsplit, shmoo, dividing);
+set(handles.Counts,'String',count_string);
+
+
+    
+
+
+% --- Executes during object creation, after setting all properties.
+function Counts_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to Counts (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
